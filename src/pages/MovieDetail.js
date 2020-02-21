d9720f7dc33c1cd50c9164c8c02d1540a502b991
kasmr <alikkras@gmail.com>
2020-02-22 01:43:12 +0300

    a bit of scss

--- src/pages/MovieDetail.js
+++ src/pages/MovieDetail.js
@@ -1,6 +1,6 @@
 import React, { useContext, useEffect } from 'react';
 import { MovieContext } from '../context/MovieContext';
-import '../index.scss';
+import '../movieDetails.scss';
 
 const MovieDetail = match => {
   const { movie, setMovie, trailers, setTrailers } = useContext(MovieContext);
@@ -32,7 +32,6 @@ const MovieDetail = match => {
   };
 
   const {
-    id,
     title,
     genres,
     original_title,
@@ -47,69 +46,73 @@ const MovieDetail = match => {
     production_countries,
     tagline
   } = movie;
-  // const style = {
-  //   backgroundImage: `https://image.tmdb.org/t/p/original${movie.backdrop_path}`,
-  //   backgroundRepeat: ' no-repeat',
-  //   backgroundPosition: 'center',
-  //   backgroundSize: 'cover',
-  //   height: '100vh',
-  //   width: '100vw'
-  // };
 
   return (
     <div
-    // className='movieDetails'
-    // style={{
-    //   backgroundImage: `url(https://image.tmdb.org/t/p/w600_and_h900_bestv2/${movie.backdrop_path})`
-    // }}
+      className='background-image'
+      style={{
+        backgroundImage: `url(https://image.tmdb.org/t/p/w600_and_h900_bestv2/${movie.backdrop_path})`,
+        backgroundRepeat: 'no-repeat',
+        backgroundSize: 'cover',
+        backgroundPosition: 'left'
+      }}
     >
-      {poster_path && (
-        <img src={`https://image.tmdb.org/t/p/w500${poster_path}`} />
-      )}
-      <h1>{title}</h1>
-      {title === original_title ? null : <h4>{original_title}</h4>}
-      <p>{tagline}</p>
-      <ul>
-        {production_countries &&
-          production_countries.map(country => (
-            <li key={country.iso_3166_1}>{country.name}</li>
-          ))}
-      </ul>
-      <ul>
-        {genres && genres.map(genre => <li key={genre.id}>{genre.name}</li>)}
-      </ul>
-      <div>
-        {production_companies &&
-          production_companies.map(company => (
-            <div key={company.id}>
-              {company.logo_path === null ? null : (
-                <img
-                  src={`https://image.tmdb.org/t/p/w200${company.logo_path}`}
-                />
-              )}
-            </div>
-          ))}
-      </div>
-      <h4>Duration: {runtime} min</h4>
-      <h4>Release date {release_date}</h4>
-      {movie.homepage ? (
-        <h4>
-          Homepage of the movie <a href={homepage}>{homepage}</a>
-        </h4>
-      ) : null}
-      <h4>
-        Average rating: {vote_average} votes: {vote_count}
-      </h4>
-      <p>{overview}</p>
-      <div>
-        <h3>Trailer</h3>
-        <iframe
-          key={trailers.id}
-          width='560'
-          height='315'
-          src={`https://www.youtube.com/embed/${trailers.key}`}
-          allow='accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture; fullscreen'
-        ></iframe>
+      <div className='movieDetails'>
+        {poster_path && (
+          <img
+            className='poster'
+            src={`https://image.tmdb.org/t/p/w500${poster_path}`}
+          />
+        )}
+        <div className='content'>
+          <h1>{title}</h1>
+          {title === original_title ? null : <h4>{original_title}</h4>}
+          <p>{tagline}</p>
+          <ul>
+            {production_countries &&
+              production_countries.map(country => (
+                <li key={country.iso_3166_1}>{country.name}</li>
+              ))}
+          </ul>
+          <ul>
+            {genres &&
+              genres.map(genre => <li key={genre.id}>{genre.name}</li>)}
+          </ul>
+          <ul>
+            {production_companies ? <h4>Production companies:</h4> : null}
+            {production_companies &&
+              production_companies.map(company => (
+                <div style={{ backgroundColor: '#fff' }} key={company.id}>
+                  {company.logo_path === null ? null : (
+                    <img
+                      src={`https://image.tmdb.org/t/p/w200${company.logo_path}`}
+                    />
+                  )}
+                </div>
+              ))}
+          </ul>
+          <h4>Duration: {runtime} min</h4>
+          <h4>Release date {release_date}</h4>
+          {movie.homepage ? (
+            <h4>
+              Homepage of the movie <a href={homepage}>{homepage}</a>
+            </h4>
+          ) : null}
+          <h4>
+            Average rating: {vote_average} votes: {vote_count}
+          </h4>
+          <p>{overview}</p>
+          <div>
+            <h3>Trailer</h3>
+            <iframe
+              key={trailers.id}
+              width='560'
+              height='315'
+              src={`https://www.youtube.com/embed/${trailers.key}`}
+              allow='accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture; fullscreen'
+            ></iframe>
+          </div>
+        </div>
       </div>
     </div>
   );
